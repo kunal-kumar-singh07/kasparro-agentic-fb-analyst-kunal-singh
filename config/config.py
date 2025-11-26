8940d1d4579796eed3317c6fb567f6f82d73a787
Kunal Kumar Singh <singhkunaliitianbusiness1@gmail.com>
2025-11-26 12:44:19 +0530

    config

--- config/config.py
+++ config/config.py
@@ -1,4 +1,15 @@
+import yaml
 import os
 
-RESULTS_DIR = r"E:\Kasparo\kasparro-agentic-fb-analyst-kunal-singh\reports"
-DATA_CSV_PATH = r"E:\Kasparo\kasparro-agentic-fb-analyst-kunal-singh\data\synthetic_fb_ads_undergarments.csv"
+CONFIG_PATH = os.path.join("config", "config.yaml")
+
+with open(CONFIG_PATH, "r") as f:
+    cfg = yaml.safe_load(f)
+
+RESULTS_DIR = cfg["paths"]["results_dir"]
+DATA_CSV_PATH = cfg["paths"]["dataset"]
+LOGS_DIR = cfg["paths"]["logs_dir"]
+
+LLM_CONFIG = cfg["llm"]
+THRESHOLDS = cfg["thresholds"]
+SEED = cfg["seed"]
